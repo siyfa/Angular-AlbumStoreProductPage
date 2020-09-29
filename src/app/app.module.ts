342432c8cfde0fc7f4db3fb39032eb3266ec7aae
siyfa <54291994+siyfa@users.noreply.github.com>
2020-09-29 15:15:15 +0100

    Completed Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,9 @@ import { ProductTracklistingComponent } from "./product-tracklisting/product-tra
 import { ProductListComponent } from "./product-list/product-list.component";
 
 const appRoutes: Routes = [
-  { path: "product", component: ProductListComponent },
+  { path: "products", component: ProductListComponent },
   { path: "product/:id", component: ProductPageComponent },
-  { path: "", redirectTo: "product", pathMatch: "full" },
+  { path: "", redirectTo: "products", pathMatch: "full" },
 ];
 
 @NgModule({
