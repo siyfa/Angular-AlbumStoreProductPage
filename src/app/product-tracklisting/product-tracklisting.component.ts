87cbf6ee236c24d3c3664b9109a5a82e6430c00d
siyfa <54291994+siyfa@users.noreply.github.com>
2020-09-29 10:47:09 +0100

    Completed Module

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../product.service";
+import { Album } from "../album";
+
+@Component({
+  selector: "app-product-tracklisting",
+  templateUrl: "./product-tracklisting.component.html",
+  styleUrls: ["./product-tracklisting.component.css"],
+})
+export class ProductTracklistingComponent implements OnInit {
+  albumInfo: any;
+  constructor(private _productSerivce: ProductService) {}
+
+  ngOnInit() {
+    this._productSerivce
+      .getAlbum(1)
+      .subscribe((response) => (this.albumInfo = response));
+  }
+}
