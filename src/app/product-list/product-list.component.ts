ffd30c5d399ee790d1f337aa4bc4be945b178731
siyfa <54291994+siyfa@users.noreply.github.com>
2020-09-30 01:19:22 +0100

    Complete Module

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,11 +10,9 @@ import { Product } from "../product";
 export class ProductListComponent implements OnInit {
   products: Product[];
 
-  constructor(private _productService: ProductService) {}
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService
-      .getProducts()
-      .subscribe((response) => (this.products = response));
+    this._productService.getProducts().subscribe((response) => (this.products = response));
   }
 }
